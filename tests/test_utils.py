117b427ffb0c57fc86b979f7f498955519ce9d34
BenTenmann <benji.tenmann@me.com>
2022-04-11 22:58:11 +0100

    chore(refactor): remove redundant functions + obj

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,4 @@
 import pytest
-import torch
 
 from protein_classification import utils
 from protein_classification.dataset import Tokenizer
@@ -16,23 +15,7 @@ def test_set_seed():
     utils.set_seed()
 
 
-def test_load_dataloader(mock_read_jsonl, tokenizer, label_map, mock_source_type):
-    tk = tokenizer()
-    lb = label_map()
-    loader = utils.load_dataloader('some/path.jsonl', tk, lb, batch_size=1, max_length=7)
-
-    X, y = next(iter(loader))
-    assert (X == torch.tensor([[1, 1, 2, 3, 0, 0, 0]], dtype=torch.long)).all()
-    assert (y == torch.tensor([0], dtype=torch.long)).all()
-
-
 def test_load_tokenizer(mock_read_json):
     tok = utils.load_tokenizer('some/path')
     assert type(tok) == Tokenizer
     assert hasattr(tok, 'token_map')
-
-
-def test_now():
-    tp = utils.now()
-
-    assert type(tp) == str
