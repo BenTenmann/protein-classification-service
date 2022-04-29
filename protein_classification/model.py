2bc3743254bfeb0240352e0adc37ca161c29ba6d
BenTenmann <benji.tenmann@me.com>
2022-04-29 13:05:43 +0100

    feat(flax): add flax resnet model

--- protein_classification/model.py
+++ protein_classification/model.py
@@ -1,377 +1,104 @@
-import math
+from typing import Sequence
 
-import torch
-from torch import nn
-from transformers import AutoModelForSequenceClassification
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
 
 __all__ = [
-    'TransformerClassifier',
-    'LanguageModelClassifier',
-    'ConvolutionClassifier',
-    'MLP',
-    'MLPOneHot'
+    'ResNet'
 ]
 
 
-class FlattenBatch(nn.Module):
+class ResidualBlock(nn.Module):
     """
-    Thin nn.Module wrapper for flattening a tensor of shape :math:`(N, L, H)` to :math:`(N, L \times H)` and then
-    executing the module.
+    Flax implementation of a residual block from the ResNet architecture [1]. Uses ReLU activation, batch norm and a
+    dilated convolution.
+
+    References
+    ----------
+    [1] He, K., Zhang, X., Ren, S. and Sun, J., 2016. Deep residual learning for image recognition. In Proceedings of
+        the IEEE conference on computer vision and pattern recognition (pp. 770-778).
 
     Attributes
     ----------
-    module: nn.Module
-        The module which will be executed on the flattened tensors.
-
-    Examples
-    --------
-    >>> model = nn.Linear(64, 1)
-    >>> model = FlattenBatch(model)
-    >>> X = torch.randn(10, 8, 8)
-    >>> y_hat = model(X)
+    input_features: int
+        The number of incoming channels for the (dilated) convolution layer.
+    block_features: int
+        The number channels internal to the block i.e. the ones passed into the second (bottleneck) convolution layer.
+        Generally, `input_features < block_features`.
+    kernel_size: Sequence[int]
+        A sequence of integers defining the kernel size for both convolution layers.
+    dilation: int
+        The size of the kernel dilation of the first convolution layer. If dilation is 1, then there is no dilation.
+    use_running_avg: bool
+        Define whether batch norm layers should use the running average. Set to `True` for model evaluation.
+        Default=False
     """
-    def __init__(self, module: nn.Module):
-        super(FlattenBatch, self).__init__()
-        self.module = module
+    input_features: int
+    block_features: int
+    kernel_size: Sequence[int]
+    dilation: int
+    padding: str
+    use_running_avg: bool = False
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch_size = x.size(0)
-        out = self.module(x.view(batch_size, -1))
-        return out
+    @nn.compact
+    def __call__(self, x):
+        norm = nn.BatchNorm(use_running_average=self.use_running_avg)(x)
+        act = nn.relu(norm)
+        conv = nn.Conv(
+            features=self.block_features,
+            kernel_size=self.kernel_size,
+            padding=self.padding.upper(),
+            kernel_dilation=self.dilation
+        )(act)
+        norm = nn.BatchNorm(use_running_average=self.use_running_avg)(conv)
+        act = nn.relu(norm)
+        conv = nn.Conv(
+            features=self.input_features,
+            kernel_size=self.kernel_size,
+            padding=self.padding.upper()
+        )(act)
+        return conv + x
 
 
-class PositionalEncoding(nn.Module):
+class ResNet(nn.Module):
     """
-    Positional encoding as defined in [1]. Adapted from `https://pytorch.org/tutorials/beginner/transformer_tutorial.html`
+    Flax implementation of the ResNet architecture [1]. Embeds integer sequences of shape :math:`(N, L)`, followed by a
+    number of residual blocks, max pooling along the sequence dimension, linear projection and log-softmax
+    normalization.
 
     References
     ----------
-    [1] Vaswani, A., Shazeer, N., Parmar, N., Uszkoreit, J., Jones, L., Gomez, A.N., Kaiser, Ł. and Polosukhin, I.,
-        2017. Attention is all you need. Advances in neural information processing systems, 30.
-    """
-    def __init__(self, d_model: int, max_len: int):
-        super(PositionalEncoding, self).__init__()
-        position = torch.arange(max_len).unsqueeze(0).unsqueeze(-1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(max_len * 2) / d_model))
-        pe = torch.zeros(1, max_len, d_model)
-        pe[0, :, 0::2] = torch.sin(position * div_term)
-        pe[0, :, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe)
+    [1] He, K., Zhang, X., Ren, S. and Sun, J., 2016. Deep residual learning for image recognition. In Proceedings of
+        the IEEE conference on computer vision and pattern recognition (pp. 770-778).
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x + self.pe
+    Attributes
+    ----------
+    num_embeddings: int
+        Number of input classes to be one-hot encoded.
+    embedding_dim: int
+        Dimension the one-hot class labels will be projected into. Must match with the input features on the residual
+        block.
+    residual_block_def: dict
+        Definition of residual block. See `ResidualBlock`.
+    n_residual_blocks: int
+        Number of residual blocks.
+    num_labels: int
+        The number of output classes.
+    """
+    num_embeddings: int
+    embedding_dim: int
+    residual_block_def: dict
+    n_residual_blocks: int
+    num_labels: int
+
+    @nn.compact
+    def __call__(self, x):
+        x = jax.nn.one_hot(x, num_classes=self.num_embeddings)
+        x = nn.Dense(self.embedding_dim)(x)
+        for _ in range(self.n_residual_blocks):
+            x = ResidualBlock(**self.residual_block_def)(x)
+        x = jnp.max(x, axis=1)
+        x = nn.Dense(features=self.num_labels)(x)
+        x = nn.log_softmax(x)
         return x
-
-
-class TransformerClassifier(nn.Module):
-    """
-    Transformer Encoder as defined in [1] with a classification head.
-
-    References
-    ----------
-    [1] Vaswani, A., Shazeer, N., Parmar, N., Uszkoreit, J., Jones, L., Gomez, A.N., Kaiser, Ł. and Polosukhin, I.,
-        2017. Attention is all you need. Advances in neural information processing systems, 30.
-    """
-    _pooling_fn = {'amax', 'mean'}
-
-    def __init__(self,
-                 n_tokens: int,
-                 d_model: int,
-                 seq_len: int,
-                 n_head: int,
-                 dim_ff: int,
-                 n_layers: int,
-                 num_labels: int,
-                 dropout: float,
-                 pooling_fn: str):
-        """
-        Initialise a TranformerClassifier object.
-
-        Parameters
-        ----------
-        n_tokens: int
-            The number of tokens, i.e. the vocabulary size.
-        d_model: int
-            The dimension of the token embeddings.
-        seq_len: int
-            The sequence length.
-        n_head: int
-            The number of heads in each Transformer encoder layer.
-        dim_ff: int
-            Feedforward dimension in each Transformer encoder layer.
-        n_layers: int
-            The number of Transformer encoder layers.
-        num_labels: int
-            The number of output classes.
-        """
-        super(TransformerClassifier, self).__init__()
-        self.d_model = d_model
-        self.embed_layer = nn.Embedding(num_embeddings=n_tokens, embedding_dim=d_model)
-        self.pos_enc = PositionalEncoding(d_model, max_len=seq_len)
-        self.transformer = nn.TransformerEncoder(
-            encoder_layer=nn.TransformerEncoderLayer(
-                d_model=d_model,
-                nhead=n_head,
-                dim_feedforward=dim_ff,
-                batch_first=True,
-                dropout=dropout
-            ),
-            num_layers=n_layers
-        )
-        if pooling_fn not in self._pooling_fn:
-            raise ValueError(f'{pooling_fn} is not a valid pooling function')
-
-        self.pooling = getattr(torch, pooling_fn)
-        self.projection = nn.Linear(in_features=d_model, out_features=num_labels)
-        self.init_weights()
-
-    def init_weights(self):
-        init_range = 0.1
-        self.embed_layer.weight.data.uniform_(-init_range, init_range)
-        self.projection.weight.data.uniform_(-init_range, init_range)
-        self.projection.bias.data.zero_()
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        mask = (x == 0)
-        embedding = self.embed_layer(x) * math.sqrt(self.d_model)
-        positional_encoding = self.pos_enc(embedding)
-        trf_embedding = self.transformer(positional_encoding, src_key_padding_mask=mask)
-        pooled_representation = self.pooling(trf_embedding, dim=1)
-        logits = self.projection(pooled_representation)
-        return logits
-
-
-class LanguageModelClassifier(nn.Module):
-    """
-    Language Model classifier (BERT) [1]. Uses pretrained language model from HuggingFace model hub [2] for fine-tuning
-    on classification task.
-
-    References
-    ----------
-    [1] Devlin, J., Chang, M.W., Lee, K. and Toutanova, K., 2018. Bert: Pre-training of deep bidirectional transformers
-        for language understanding. arXiv preprint arXiv:1810.04805.
-    [2] Wolf, T., Debut, L., Sanh, V., Chaumond, J., Delangue, C., Moi, A., Cistac, P., Rault, T., Louf, R., Funtowicz,
-        M. and Davison, J., 2019. Huggingface's transformers: State-of-the-art natural language processing.
-        arXiv preprint arXiv:1910.03771. https://huggingface.co/models
-
-    Examples
-    --------
-    >>> model = LanguageModelClassifier('Rostlab/prot_bert')  # downloads pretrained ProtBert model
-    """
-    def __init__(self, identifier: str, freeze_bert: bool = False, **kwargs):
-        """
-        Initialize a LanguageModelClassifier object.
-
-        Parameters
-        ----------
-        identifier: str
-            The model hub [2] identifier string for the pre-trained language model.
-        freeze_bert: bool
-            Boolean defining whether to deactivate gradients (i.e. "freeze") the BERT layers. If set to `True`, then
-            only the classification head parameters will be updated. (default=False)
-        kwargs
-            Keyword arguments passed onto `AutoModelForSequenceClassification.from_pretrained`.
-        """
-        super(LanguageModelClassifier, self).__init__()
-        self.model = AutoModelForSequenceClassification.from_pretrained(identifier, **kwargs)
-        for param in self.model.bert.parameters():
-            param.requires_grad = not freeze_bert
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        out = self.model(x).logits
-        return out
-
-
-class MLP(nn.Module):
-    """
-    Multi-layer Perceptron [1] for classification task.
-
-    References
-    ----------
-    [1] Haykin, S., 1994. Neural networks: a comprehensive foundation, Prentice Hall PTR.
-    """
-    def __init__(self,
-                 d_model: int,
-                 seq_len: int,
-                 hidden_dim: int,
-                 num_labels: int,
-                 negative_slope: float,
-                 dropout: float,
-                 flatten_first: bool = False):
-        """
-        Initialise a basic MLP.
-
-        Parameters
-        ----------
-        d_model: int
-            The dimension of the token features.
-        seq_len: int
-            The sequence length.
-        hidden_dim: int
-            The dimension of the hidden layer.
-        num_labels: int
-            The number of output classes.
-        negative_slope: float
-            The negative slope of the leaky ReL unit.
-        dropout: float
-            The unit dropout rate.
-        flatten_first: Optional[bool]
-            Whether to flatten the sequence dimension (= concatenate tokens) before the first layer (`True`), or the
-            before the second (`False`; default).
-        """
-        super(MLP, self).__init__()
-        seq_len_1 = seq_len ** flatten_first  # if True, multiply the number of input dimensions
-        self.proj_1 = nn.Linear(in_features=int(d_model * seq_len_1),
-                                out_features=hidden_dim)
-        self.l_relu = nn.LeakyReLU(negative_slope)
-        seq_len_2 = seq_len ** (not flatten_first)  # otherwise, multiply the number of hidden dimensions
-        self.proj_2 = nn.Linear(in_features=int(hidden_dim * seq_len_2),
-                                out_features=num_labels)
-        self.dropout = nn.Dropout(dropout)
-
-        if flatten_first:
-            self.proj_1 = FlattenBatch(self.proj_1)
-        else:
-            self.proj_2 = FlattenBatch(self.proj_2)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        MLP forward pass.
-
-        Parameters
-        ----------
-        x: torch.Tensor
-            Input tensor of shape :math:`(N, L, H)`, where :math:`L` is the sequence length and :math:`H` is the token
-            feature dimension.
-
-        Returns
-        -------
-        out: torch.Tensor
-            The unnormalised class scores of shape :math:`(N, C)`, where the :math:`C` is the number of classes.
-        """
-        x = self.dropout(x)
-        proj = self.proj_1(x)
-        act = self.l_relu(proj)
-        out = self.proj_2(act)
-        return out
-
-
-class MLPOneHot(nn.Module):
-    """
-    Multi-layer Perceptron [1] for classification. Unlike `MLP`, `MLPOneHot` takes a sequence of class indices rather
-    than a sequence of token embeddings.
-
-    References
-    ----------
-    [1] Haykin, S., 1994. Neural networks: a comprehensive foundation, Prentice Hall PTR.
-    """
-    def __init__(self,
-                 n_tokens: int,
-                 embed_dim: int,
-                 seq_len: int,
-                 num_labels: int,
-                 negative_slope: float,
-                 dropout: float):
-        """
-        Initialise an MLPOneHot object.
-
-        Parameters
-        ----------
-        n_tokens: int
-            The number of tokens, i.e. the size of the vocabulary.
-        embed_dim: int
-            The token embedding dimension.
-        seq_len: int
-            The sequence length.
-        num_labels:
-            The number of output classes.
-        negative_slope:
-            The negative slope of the leaky ReL unit.
-        dropout: float
-            The unit dropout probability.
-        """
-        super(MLPOneHot, self).__init__()
-        self.embed = nn.Embedding(num_embeddings=n_tokens, embedding_dim=embed_dim)
-        self.l_relu = nn.LeakyReLU(negative_slope)
-        self.proj = nn.Linear(in_features=embed_dim * seq_len, out_features=num_labels)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch_size = x.size(0)
-        embedding = self.dropout(self.embed(x))
-        act = self.l_relu(embedding)
-        out = self.proj(act.view(batch_size, -1))
-        return out
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self,
-                 input_channels: int,
-                 block_channels: int,
-                 kernel_size: tuple,
-                 dilation: tuple,
-                 padding: tuple or str):
-        super(ResidualBlock, self).__init__()
-        self.batch_norm_1 = nn.BatchNorm1d(input_channels)
-        self.relu = nn.ReLU()
-        self.dilated_convolution = nn.Conv1d(
-            in_channels=input_channels,
-            out_channels=block_channels,
-            kernel_size=kernel_size,
-            dilation=dilation,
-            padding=padding
-        )
-        self.batch_norm_2 = nn.BatchNorm1d(block_channels)
-        self.bottleneck_convolution = nn.Conv1d(
-            in_channels=block_channels,
-            out_channels=input_channels,
-            kernel_size=kernel_size,
-            padding=padding
-        )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        norm_1 = self.batch_norm_1(x)
-        act_1 = self.relu(norm_1)
-        conv_1 = self.dilated_convolution(act_1)
-        norm_2 = self.batch_norm_2(conv_1)
-        act_2 = self.relu(norm_2)
-        conv_2 = self.bottleneck_convolution(act_2)
-        out = conv_2 + x
-        return out
-
-
-class ConvolutionClassifier(nn.Module):
-    _pooling_fn: set = {'amax', 'mean'}
-
-    def __init__(self,
-                 num_embeddings: int,
-                 embedding_dim: int,
-                 n_residual_blocks: int,
-                 residual_block_def: dict,
-                 num_labels: int,
-                 pooling: str = 'max'
-                 ):
-        super(ConvolutionClassifier, self).__init__()
-        self.embed = nn.Embedding(num_embeddings=num_embeddings, embedding_dim=embedding_dim)
-        residual_block_def.update({'input_channels': embedding_dim})
-        residual_blocks = [ResidualBlock(**residual_block_def) for _ in range(n_residual_blocks)]
-        self.residual_blocks = nn.Sequential(*residual_blocks)
-        if pooling not in self._pooling_fn:
-            raise ValueError(f'{repr(pooling)} is not a valid pooling function')
-
-        self.pooling = getattr(torch, pooling)
-        self.projection = nn.Linear(
-            in_features=embedding_dim,
-            out_features=num_labels
-        )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        embedding = self.embed(x)
-        (N, L, C) = embedding.size()
-        # the 1d convolution is done over the sequence dimension, so we need to have the sequence dimension as the last
-        # dimension
-        embedding = embedding.view(N, C, L)
-        embedding = self.residual_blocks(embedding)
-        embedding = self.pooling(embedding, dim=-1)
-        out = self.projection(embedding)
-        return out
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,75 @@
 import pytest
 
-import torch
+import jax
 
 
 @pytest.fixture
-def mock_read_json(monkeypatch):
+def rng():
+    init_rng = jax.random.PRNGKey(0)
+    return init_rng
+
+
+@pytest.fixture
+def array_shape():
+    shape = (1024, 256)
+    return shape
+
+
+@pytest.fixture
+def mock_jnp_load(monkeypatch, rng, array_shape):
     monkeypatch.setattr(
-        'protein_classification.utils.srsly.read_json',
-        lambda *args, **kwargs: {'A': 1, 'S': 2, 'Q': 3}
+        'protein_classification.utils.jnp.load',
+        lambda *args, **kwargs: jax.random.randint(rng, array_shape, 0, 25)
     )
 
 
 @pytest.fixture
-def mock_read_jsonl(monkeypatch):
+def mock_jnp_load_2(monkeypatch, rng):
+    import jax
+
+    def jnp_load(file_path: str) -> jax.numpy.ndarray:
+        shape, n_classes = ((128, 256), 25) if 'source' in file_path else ((128, 1), 17_929)
+        mock_array = jax.random.randint(rng, shape, 0, n_classes)
+        return mock_array
+
     monkeypatch.setattr(
-        'protein_classification.utils.srsly.read_jsonl',
-        lambda *args, **kwargs: [{'sequence': 'AASQ', 'family_accession': 'XX'}]
+        'protein_classification.utils.jnp.load',
+        jnp_load
     )
 
 
 @pytest.fixture
-def label_map():
-    def _method():
-        return {'XX': 0}
-    return _method
+def mock_read_bytes(monkeypatch):
+    monkeypatch.setattr(
+        'protein_classification.utils.Path.read_bytes',
+        lambda *args, **kwargs: b''
+    )
 
 
 @pytest.fixture
-def mock_source_type(monkeypatch):
+def mock_from_bytes(monkeypatch):
     monkeypatch.setattr(
-        'protein_classification.dataset.ProteinFamilyDataset.source_type',
-        torch.long
+        'protein_classification.utils.serialization.from_bytes',
+        lambda *args, **kwargs: {'params': {}, 'batch_stats': {}}
     )
+
+
+@pytest.fixture
+def mock_wandb(monkeypatch):
+    monkeypatch.setattr(
+        'protein_classification.execution.wandb.log',
+        lambda *args, **kwargs: None
+    )
+    monkeypatch.setattr(
+        'protein_classification.execution.wandb.init',
+        lambda *args, **kwargs: None
+    )
+
+
+@pytest.fixture
+def base_model(array_shape, rng):
+    from protein_classification import model, constants
+    resnet = model.ResNet(**constants.MODEL_CONF)
+    batch = jax.numpy.ones(array_shape)
+    params = resnet.init(rng, batch)
+    return resnet, params
