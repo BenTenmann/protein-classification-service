4c03d014d8dbd5e89d39def43648dffb4f5f2da5
BenTenmann <benji.tenmann@me.com>
2022-04-29 13:52:58 +0100

    chore(cleanup): clean old torch comps

--- scripts/reformat-pfam-dataset.sh
+++ scripts/reformat-pfam-dataset.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
+# set the `DATA_ROOT_DIR` environment variable to point at the directory which contains the pfam-seed random split
+# subdirectories. By default the script will assume that the untarred folders have been placed in the $REPO_ROOT/data
+# directory and will use the `$REPO_ROOT/data/random_split/random_split` subdir.
+
 set -e
 
-DIR="$1"
-OUT="$2"
+REPO_ROOT="$(dirname "$0")"/..
 
-if [[ -z ${DIR} ]]; then
-    DIR="${PWD}/data"
+PYTHONENV="$REPO_ROOT"/venv
+if [[ -d $PYTHONENV ]]; then
+  echo error: no python3 virtualenv detected.
+  exit 1
 fi
 
-if [[ -z ${OUT} ]]; then
-    OUT="${DIR}"
-fi
+export PATH="$PYTHONENV"/bin:"$PATH"
+SCRIPT="$REPO_ROOT"/scripts/reformat_pfam.py
 
-PYPATH="${PWD}"/venv/bin
-SCRIPT="${PWD}"/scripts/reformat_pfam.py
-
-export OUTPUT="${OUT}"
-for subdir in "${DIR}"/*
+export OUTPUT_DIR="${OUTPUT_DIR:-"$REPO_ROOT"/data}"
+for subdir in "${DATA_ROOT_DIR:-"$REPO_ROOT"/data/random_split/random_split}"/*
 do
-    export INPUT="${subdir}"
-    "${PYPATH}"/python3 "${SCRIPT}"
+    export INPUT_DIR="$subdir"
+    python3 "$SCRIPT"
 
-    cat "${subdir}"/*.jsonl > "${OUT}"/"$(basename "${subdir}")".jsonl
-    rm "${subdir}"/*.jsonl
+    cat "$subdir"/*.jsonl > "$OUTPUT_DIR/$(basename "$subdir")".jsonl
+    rm "$subdir"/*.jsonl
 done
--- scripts/reformat_pfam.py
+++ scripts/reformat_pfam.py
@@ -1,3 +1,15 @@
+"""
+This script reformats the pfam-seed random split dataset downloaded from kaggle [1] from csv format into jsonl format.
+The original data has each data split as a subdirectory of multiple csv files, hence we make them into one jsonl file
+per split for ease of use. Each csv is made into a jsonl, which can then easily be concatenated in the shell.
+
+This script is used by `$REPO_ROOT/scripts/reformat-pfam-dataset.sh`.
+
+References
+----------
+[1] PFAM seed random split: https://www.kaggle.com/datasets/googleai/pfam-seed-random-split
+"""
+
 import logging
 import os
 from pathlib import Path
@@ -11,13 +23,13 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-INPUT = os.environ.get('INPUT')
-OUTPUT = os.environ.get('OUTPUT')
+INPUT_DIR = os.environ.get('INPUT_DIR')
+OUTPUT_DIR = os.environ.get('OUTPUT_DIR')
 
 
 def main():
-    input_path = Path(INPUT)
-    output_path = Path(OUTPUT)
+    input_path = Path(INPUT_DIR)
+    output_path = Path(OUTPUT_DIR)
 
     logging.debug(f'input path: {input_path}')
     logging.debug(f'output path: {output_path}')
